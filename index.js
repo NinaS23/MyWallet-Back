ccebbb9e592934bae8d8b4ed51e61a6dbdf8f963
MariaDuda <mariabevenuto062@gmail.com>
2022-06-29 11:43:18 -0300

    login e cadastro funcionando de buenas

--- index.js
+++ index.js
@@ -26,63 +26,64 @@ promise.catch(res => console.log(chalk.red("deu xabu"), res))
 
 
 app.post("/" , async (req, res) =>{
-    //req.body 
+  
     const user = req.body   
-    //validar com joi se email é uma string vazia e senha string vazia
+
     const userSchema = joi.object({
         email:joi.string().email().required() ,
         senha: joi.string().min(4).required()
     });
+
     const { error } = userSchema.validate(user)
     if (error) {
         return res.status(400).send("email ou senha incorretos")
     }
+    try{
 
-    const acharUser = await db.collection("users").findOne({ email: user.email })
-    const compararSenhas = bcypt.compareSync(user.senha, acharUser.senha)
-    if (!acharUser || !compararSenhas) {
-        console.log(acharUser.senha)
-        return res.status(400).send("email ou senha incorretos")
-    }else{
-       const token = uuid();
-       await db.collection("sessions").insertOne({
-           userId: acharUser._id,
-           token
-       })
-       return res.send(token)
+        const acharUser = await db.collection("users").findOne({ email: user.email })
+        const compararSenhas = bcypt.compareSync(user.senha, acharUser.senha)
+        if (!acharUser || !compararSenhas) {
+            console.log(acharUser.senha)
+            return res.status(400).send("email ou senha incorretos")
+        }else{
+           const token = uuid();
+           await db.collection("sessions").insertOne({
+               userId: acharUser._id,
+               token
+           })
+           return res.send(token)
+        }
+    }catch(e){
+        console.log(e)
     }
-    res.status(201).send("CREATED")
-
-    //se n for , comparar a senha e fazer um findeONe no email e ver se o user exist 
-    //se n exite validar
-    // se existe colocar no banco de dados com senha criptografada
-    //uuid caso exista o user
 })
 
 app.post("/cadastrar" , async (req , res)=>{
-    //req.body (name , email , senha , url)
+
     const { nome , email , senha , url} = req.body
-    //joi pra validar cada um 
+
     const cadastroSchema = joi.object({
         nome:joi.string().required(),
         email:joi.string().email().required() ,
         senha: joi.string().min(4).required(),
-        url:joi.string().required()
+        url:joi.string().required()//usar regex pra validar a url
     });
     const { error } = cadastroSchema.validate({nome , email , senha , url})
     if(error){
         return res.status(400).send("sorry")
     }
-    const verificaEmail = await db.collection("users").findOne({email})
-    if(verificaEmail){
-        return res.status(400).send("tente outro email")
+    try{
+        const verificaEmail = await db.collection("users").findOne({email})
+        if(verificaEmail){
+            return res.status(400).send("tente outro email")
+        }
+        const criptograarSenha = bcypt.hashSync(senha , 10)
+        await db.collection('users').insertOne({ nome , email , senha: criptograarSenha , url }) 
+        res.status(201).send("CREATED")
+    }catch(e){
+        console.log(e)
     }
-    const criptograarSenha = bcypt.hashSync(senha , 10)
-    await db.collection('users').insertOne({ nome , email , senha: criptograarSenha , url }) 
-    res.status(201).send("CREATED")
-   
-    
-    
+
 })
 
 const PORT = process.env.PORT || 5006
